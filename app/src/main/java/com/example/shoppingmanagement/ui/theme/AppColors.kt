1e18ee13311748e5efb1387e1e47d892fd5fcf57
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2026-07-02 21:40:02 +0200

    Set up Theme.kt to support dark and light mode selection. Added some temporary colors to Color.kt. Added AppColors.kt to store a shared value which returns a dark or light mode value for a color depending on the current theme.

---
+++ app/src/main/java/com/example/shoppingmanagement/ui/theme/AppColors.kt
@@ -0,0 +1,66 @@
+package com.example.shoppingmanagement.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.material3.ColorScheme
+import androidx.compose.ui.graphics.Color
+
+// Blue
+val ColorScheme.darkBlue: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) DarkBlueDark else DarkBlueLight
+
+val ColorScheme.lightBlue: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) LightBlueDark else LightBlueLight
+
+// Brown
+val ColorScheme.darkBrown: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) DarkBrownDark else DarkBrownLight
+
+val ColorScheme.lightBrown: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) LightBrownDark else LightBrownLight
+
+// Gray
+val ColorScheme.lightGray: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) LightGrayDark else LightGrayLight
+
+// Green
+val ColorScheme.darkGreen: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) DarkGreenDark else DarkGreenLight
+
+val ColorScheme.middleGreen: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) MiddleGreenDark else MiddleGreenLight
+
+val ColorScheme.lightGreen: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) LightGreenDark else LightGreenLight
+
+// Red
+val ColorScheme.darkRed: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) DarkRedDark else DarkRedLight
+
+val ColorScheme.middleRed: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) MiddleRedDark else MiddleRedLight
+
+val ColorScheme.lightRed: Color
+    @Composable
+    @ReadOnlyComposable
+    get() = if (LocalIsDarkTheme.current) LightRedDark else LightRedLight
--- app/src/main/java/com/example/shoppingmanagement/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppingmanagement/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.example.shoppingmanagement.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Temporary colors for initial testing and setup
+// Light and dark theme of some colors are temporarily the same until real colors are picked
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBlueLight = Color(0xFF5C80BC)
+val DarkBlueDark = Color(0xFF5C80BC)
+val LightBlueLight = Color(0xFFD4DEED)
+val LightBlueDark = Color(0xFFD4DEED)
+val DarkBrownLight = Color(0xFF311E10)
+val DarkBrownDark = Color(0xFF311E10)
+val LightBrownLight = Color(0xFFFAF4F0)
+val LightBrownDark = Color(0xFFFAF4F0)
+val LightGrayLight = Color(0xFFE3E3E3)
+val LightGrayDark = Color(0xFFE3E3E3)
+
+val DarkGreenLight = Color(0xFF1E9E4C)
+val DarkGreenDark = Color(0xFF0F5E27)
+val MiddleGreenLight = Color(0xFFCFF1D9)
+val MiddleGreenDark = Color(0xFF2E7D32)
+val LightGreenLight = Color(0xFFF0FCF3)
+val LightGreenDark = Color(0xFF1B3B1B)
+
+val DarkRedLight = Color(0xFFB91C1C)
+val DarkRedDark = Color(0xFF7F0F0F)
+val MiddleRedLight = Color(0xFFFECACA)
+val MiddleRedDark = Color(0xFFC62828)
+val LightRedLight = Color(0xFFFFF1F1)
+val LightRedDark = Color(0xFF3E1B1B)
